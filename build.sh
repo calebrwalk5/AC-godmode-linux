fc3c5a2fd8753eb5862b3191bacb2712ecda5d2f
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2023-01-04 18:28:10 -0500

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ptrace_scope=$(cat /proc/sys/kernel/yama/ptrace_scope)
+
+if [ "$ptrace_scope" -eq "1" ]; then
+  sudo sysctl kernel.yama.ptrace_scope=0
+fi
+
+if which xdotool >/dev/null; then
+    echo "xdotool is installed"
+else
+    echo "xdotool is not installed, please install it with your package manager"
+fi
+
+if which g++ >/dev/null; then
+    echo "g++ is installed"
+else
+    echo "g++ is not installed, please install it with your package manager"
+fi
+
+sudo sysctl kernel.yama.ptrace_scope=0
+g++ main.cpp -o main
--- main.cpp
+++ main.cpp
@@ -1,18 +1,74 @@
 #include <iostream>
+
 #include <stdio.h>
+
 #include <string.h>
 
+#include <unistd.h>
+
+#include <sys/ptrace.h>
+
+#include <sys/wait.h>
+
+#include <sys/user.h>
+
+#include <sys/types.h>
+
+const int TARGET_VALUE = 20;
+
 int main() {
-    FILE* fp = popen("xdotool search --name \"AssaultCube\" getwindowpid", "r");
-    if (fp == NULL) {
-        std::cerr << "Failed to run xdotool" << std::endl;
-        return 1;
+  std::cout << "Written by anvsO1 (https://www.unknowncheats.me/forum/members/5175763.html)" << std::endl;
+  // Get the window ID of the Assault Cube window
+  char buf[256];
+  FILE * xdotool = popen("xdotool search --name 'AssaultCube'", "r");
+  if (!xdotool) {
+    std::cout << "Please install xdotool" << std::endl;
+    return 1;
+  } else {
+    std::cout << "xdotool found" << std::endl;
+  }
+  if (!fgets(buf, sizeof(buf), xdotool)) {
+    std::cout << "Could not find Assault Cube window" << std::endl;
+    return 1;
+  }
+  std::cout << "fgets called" << std::endl;
+  std::cout << "buf: " << buf << std::endl;
+  int window_id = atoi(buf);
+  std::cout << "window_id: " << window_id << std::endl;
+
+  // Get the PID of the ac_client process
+  snprintf(buf, sizeof(buf), "xdotool getwindowpid %d", window_id);
+  xdotool = popen(buf, "r");
+  if (!xdotool) {
+    perror("popen");
+    return 1;
+  }
+  if (!fgets(buf, sizeof(buf), xdotool)) {
+    std::cout << "Could not get PID of Assault Cube process" << std::endl;
+    return 1;
+  }
+  pid_t pid = atoi(buf);
+
+  // Attach to the ac_client process
+  if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) == -1) {
+    perror("ptrace attach");
+    return 1;
+  }
+  wait(NULL);
+  std::cout << "waiting NULL" << std::endl;
+  struct user_regs_struct regs;
+  long addr = 0x00000000;
+  std::cout << "testing for " << TARGET_VALUE << std::endl;
+  while (addr < 0xFFFFFFFF) {
+    ptrace(PTRACE_PEEKDATA, pid, addr, & regs);
+    int32_t value = *((int32_t *) &regs);
+    if (value == TARGET_VALUE) {
+    //if (regs.rax == TARGET_VALUE) {
+      std::cout << "Found target value at address: 0x" << std::hex << addr << std::endl;
+      break;
     }
-
-    char output[10];
-    fgets(output, sizeof(output)-1, fp);
-    pclose(fp);
-
-    std::cout << "PID: " << output << std::endl;
-    return 0;
+    addr += sizeof(int32_t);
+  }
+  ptrace(PTRACE_DETACH, pid, NULL, NULL);
+  return 0;
 }
