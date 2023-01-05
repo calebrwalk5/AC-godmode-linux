b776721db46a89672653703d8ad35ebd1021b963
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2023-01-05 14:48:39 -0500

    Add files via upload

--- memedit.c
+++ memedit.c
@@ -1,8 +1,8 @@
+#include <inttypes.h>
+
 #include <stdio.h>
 
 #include <stdlib.h>
-
-#include <inttypes.h>
 
 int main(int argc, char * argv[]) {
   if (argc < 3) {
@@ -10,15 +10,20 @@ int main(int argc, char * argv[]) {
     return 1;
   }
 
-  uintptr_t address = strtoull(argv[1], NULL, 16);
+  char * endptr;
+  unsigned long address = strtoul(argv[1], & endptr, 16);
+  if ( * endptr != '\0') {
+    printf("Invalid address: %s\n", argv[1]);
+    return 1;
+  }
+
   int value = atoi(argv[2]);
 
   int * ptr = (int * ) address;
 
   * ptr = value;
 
-  printf("Value %d assigned to address 0x%"
-    PRIxPTR "\n", value, address);
+  printf("Value %d assigned to address 0x%lx\n", value, address);
 
   return 0;
 }
